4c8e8174f7ffe956ad91ffcd86751f6ff4237731
yzhang2544 <85782563+yzhang2544@users.noreply.github.com>
2024-05-15 10:01:45 +0800

    Update arxiv_daily_statistics.py

--- arxiv_daily_statistics.py
+++ arxiv_daily_statistics.py
@@ -25,8 +25,8 @@ else:
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
-s = soup.find(class_="list-dateline").text.split("announced")[-1].strip()
-day, month, year = [_.strip() for _ in s.split(",")[-1].split()]
+s = soup.find("h3").text.split(",")[-1].strip().split(' ')
+day, month, year = s[0], s[1], s[2]
 info_folder = "info/%s-%s/%s" % (year, month, day)
 os.makedirs(info_folder, exist_ok=True)
 
@@ -46,8 +46,10 @@ for dl in soup.find_all("dl"):
 
     for (dd, dt) in dds_dts:
         # arXiv id
-        list_identifier = dt.find(class_="list-identifier")
-        id = list_identifier.a.text
+        #list_identifier = dt.find(class_="list-identifier")
+        #id = list_identifier.a.text
+        
+        id = dt.find(title='Abstract').text.strip()
 
         # abstract
         abstract = dd.find_all(class_="mathjax")[-1].text.replace('\n', ' ').replace('\r', ' ')
